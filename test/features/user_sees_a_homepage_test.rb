ed6bdb318f82b25b743b7410a6b8000202456f8e
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-06-16 14:34:51 -0600

    deployment commit

---
+++ test/features/user_sees_a_homepage_test.rb
@@ -0,0 +1,18 @@
+require './test/test_helper'
+require 'pry'
+
+class HomepageTest < CapybaraTestCase
+  def test_user_can_see_the_homepage
+    visit '/'
+
+    assert page.has_content?("Welcome!")
+    assert_equal 200, page.status_code
+  end
+
+  def test_user_gets_error_when_visiting_page_that_does_not_exist
+    visit 'anything'
+
+    refute page.has_content?("Welcome!")
+    assert_equal 404, page.status_code
+  end
+end
